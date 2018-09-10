cdf7a39730e133b2b4202dfd6fe44c29560728de
romero728 <david728romero@gmail.com>
2018-09-10 16:02:39 -0500

    Modificación Index
    Se ha añadido el logo de DSB
    Se ha modificado el navbar
    
    DRM

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -35,12 +35,13 @@
   <body>
 
     <h1 class="site-heading text-center text-white d-none d-lg-block">
-      <span class="site-heading-upper text-primary mb-3">Artículos únicos en su clase</span>
-      <span class="site-heading-lower">Ponte a la vanguardia</span>
+      <img src="img/logos/logoDBS.jpeg">
+      <span class="site-heading-upper text-primary mb-3">DSB</span>
+      <span class="site-heading-lower">Fabricando ideas</span>
     </h1>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav"> <!--navbar navbar-expand-lg navbar-dark py-lg-4-->
       <div class="container">
         <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">DSB FABRICANDO IDEAS</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
